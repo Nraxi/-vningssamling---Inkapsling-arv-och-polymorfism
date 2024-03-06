91a1c83b7dc0644802d91c8dccce7d310e67a5f4
Olle Tengnér <olle.code@hotmail.com>
2024-03-06 20:19:13 +0100

    Add project files.

---
+++ Övningssamling - Inkapsling, arv och polymorfism/Person.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Övningssamling___Inkapsling__arv_och_polymorfism
+{
+    class Person
+    {
+        private int age;
+        private string? fName;
+        private string? lName;
+        private double height;
+        private double weight;
+
+
+
+
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age must be greater then 0");
+                }
+            }
+        }
+
+
+        public string? FName
+        {
+            get { return fName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length >= 2 && value.Length <= 10)
+                {
+                    fName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name must betweeb 2 and 10 char long");
+                }
+            }
+        }
+
+
+        public string? LName
+        {
+            get { return lName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length >= 3 && value.Length <= 15)
+                {
+                    lName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name must be between 3 and 15 char long");
+                }
+            }
+        }
+
+
+        public Person(int age, string fName, string lName)
+        {
+            Age = age;
+            FName = fName;
+            LName = lName;
+        }
+        public override string ToString()
+        {
+            return $"Age: {Age}, First Name: {FName}, Last Name: {LName}, Height: {Height} cm, Weight: {Weight} kg";
+        }
+    }
+    class PersonHandler
+    {
+        public void SetAge(Person pers, int age)
+        {
+            pers.Age = age;
+        }
+
+        public Person CreatePerson(int age, string fName, string lName, double height, double weight)
+        {
+            Person person = new Person(age,fName,lName);
+            person.Height = height;
+            person.Weight = weight;
+            return person ;
+        }
+    }
+}
---
+++ Övningssamling - Inkapsling, arv och polymorfism/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Övningssamling___Inkapsling__arv_och_polymorfism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Create a Mandalorian Bounty Hunter
+            //    Person person = new Person(10, "Jango", "Fett");
+            //    person.Height = 183;
+            //    person.Weight = 79;
+
+            //    Console.WriteLine("Person created successfully.");
+            //    Console.WriteLine(person); 
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+
+            try
+            {
+                PersonHandler handler = new PersonHandler();
+
+                //Create a  JEDI
+                Person person = handler.CreatePerson(21, "Anakin", "Skywalker", 188, 84);
+
+                //  handler.SetAge(person, 25);
+
+                Console.WriteLine("Jedi was created:");
+                Console.WriteLine($"Name: {person.FName} {person.LName}");
+                Console.WriteLine($"Age: {person.Age}");
+                Console.WriteLine($"Height: {person.Height} cm");
+                Console.WriteLine($"Weight: {person.Weight} kg");
+            }
+            catch (ArgumentException ex) { Console.WriteLine($"Error: {ex.Message}"); }
+        }
+    }
+}
